c692fe73ceb212d849d6bf9026e670e38601b2b5
varun-kumar-meda <mkumar@phdata.io>
2024-09-05 15:52:06 +0530

    Update streamlit_app.py
    updated get session

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 # st.write("You selected:", option)
 cnx = st.connection("snowflake")
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
